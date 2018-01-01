b37ffb91dd49211c5873e5a2f6607c9cdff8bafd
Younes Khoubza <nesyou2013@gmail.com>
2018-01-01 20:53:13 +0000

    Apply fixes from StyleCI

--- src/Traits/BulkTrait.php
+++ src/Traits/BulkTrait.php
@@ -5,7 +5,7 @@ namespace Yoeunes\Larafast\Traits;
 trait BulkTrait
 {
     /**
-     * Mass (bulk) insert or update on duplicate for Laravel 4/5
+     * Mass (bulk) insert or update on duplicate for Laravel 4/5.
      *
      * insertOrUpdate([
      *   ['id'=>1,'value'=>10],
@@ -19,15 +19,23 @@ trait BulkTrait
      */
     public function insertOrUpdate(array $rows): bool
     {
-        $table = \DB::getTablePrefix().with(new static)->getTable();
+        $table = \DB::getTablePrefix().with(new static())->getTable();
 
         $first = reset($rows);
 
-        $columns = implode(',', array_map(function ($value) { return "$value"; }, array_keys($first)));
+        $columns = implode(',', array_map(function ($value) {
+            return "$value";
+        }, array_keys($first)));
 
-        $values = implode(',', array_map(function ($row) {return '('.implode(',', array_map(function ($value) { return '"'.str_replace('"', '""', $value).'"'; }, $row)).')';}, $rows));
+        $values = implode(',', array_map(function ($row) {
+            return '('.implode(',', array_map(function ($value) {
+                return '"'.str_replace('"', '""', $value).'"';
+            }, $row)).')';
+        }, $rows));
 
-        $updates = implode(',', array_map(function ($value) { return "$value = VALUES($value)"; }, array_keys($first)));
+        $updates = implode(',', array_map(function ($value) {
+            return "$value = VALUES($value)";
+        }, array_keys($first)));
 
         $sql = "INSERT INTO {$table}({$columns}) VALUES {$values} ON DUPLICATE KEY UPDATE {$updates}";
 
