adcae34f0d0592206e5f35f04b255690db0dc3be
Younes Khoubza <nesyou2013@gmail.com>
2018-01-03 11:15:00 +0000

    webController crud functions

--- src/Controllers/WebController.php
+++ src/Controllers/WebController.php
@@ -2,6 +2,10 @@
 
 namespace Yoeunes\Larafast\Controllers;
 
+use Maatwebsite\Excel\Classes\LaravelExcelWorksheet;
+use Maatwebsite\Excel\Collections\RowCollection;
+use Maatwebsite\Excel\Writers\LaravelExcelWriter;
+use Yoeunes\Larafast\Entities\Entity;
 use Yoeunes\Larafast\Policies\Policy;
 use Yoeunes\Larafast\Traits\DataTableScopeTrait;
 use Yoeunes\Larafast\Traits\DataTableTrait;
@@ -11,6 +15,21 @@ use Yoeunes\Larafast\Traits\ViewTrait;
 class WebController extends Controller
 {
     use ViewTrait, DataTableTrait, DataTableScopeTrait, ServiceTrait;
+
+    /**
+     * @return mixed
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function index()
+    {
+        if (is_a($this->policy, Policy::class, true)) {
+            $this->authorize(__FUNCTION__, $this->entityName());
+        }
+
+        return $this->getDataTable()->addScope($this->getDataTableScope())->render($this->getView(__FUNCTION__));
+    }
+
 
     /**
      * @throws \Illuminate\Auth\Access\AuthorizationException
@@ -24,5 +43,213 @@ class WebController extends Controller
         }
 
         return view($this->getView(__FUNCTION__));
+    }
+
+    /**
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function store()
+    {
+        if (is_a($this->policy, Policy::class, true)) {
+            $this->authorize(__FUNCTION__, $this->entityName());
+        }
+
+        request()->validate($this->getEntity()->getRules(__FUNCTION__), $this->getEntity()->getMessages());
+
+        $entity = $this->getService()->store(request($this->getEntity()->getFillableAttributes(__FUNCTION__)));
+
+        if ($entity instanceof Entity) {
+            return success('L\'Entité a été ajoutée avec succès');
+        }
+
+        return error('Une erreur s"est produite veuillez réessayer ultérieurement');
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function edit(int $id)
+    {
+        if (is_a($this->policy, Policy::class, true)) {
+            $this->authorize(__FUNCTION__, $this->entityName());
+        }
+
+        $entity = $this->getEntity()->findOrFail($id);
+
+        return view($this->getView(__FUNCTION__), compact('entity'));
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function show(int $id)
+    {
+        return redirect()->route(preg_replace('/\.show/', '.edit', app('router')->getCurrentRoute()->getName()), $id);
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function update(int $id)
+    {
+        if (is_a($this->policy, Policy::class, true)) {
+            $this->authorize(__FUNCTION__, $this->entityName());
+        }
+
+        /** @var Entity $entity */
+        $entity = $this->getEntity()->findOrFail($id);
+
+        request()->validate($this->getEntity()->getRules(__FUNCTION__), $this->getEntity()->getMessages());
+
+        if ($this->getService()->update($entity, request($this->getEntity()->getFillableAttributes(__FUNCTION__)))) {
+            return success('L\'Entité a été modifiée avec succès');
+        }
+
+        return error('Une erreur s\'est produite veuillez réessayer ultérieurement');
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     * @throws \Exception
+     */
+    public function destroy(int $id)
+    {
+        if (is_a($this->policy, Policy::class, true)) {
+            $this->authorize(__FUNCTION__, $this->entityName());
+        }
+
+        /** @var Entity $entity */
+        $entity = $this->getEntity()->findOrFail($id);
+
+        if ($this->getService()->destroy($entity)) {
+            return success('L\'Entité a été supprimée avec succès');
+        }
+
+        return error('Une erreur s\'est produite veuillez réessayer ultérieurement');
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function excelCreate()
+    {
+        if (is_a($this->policy, Policy::class, true)) {
+            $this->authorize(__FUNCTION__, $this->entityName());
+        }
+
+        return view($this->getView(__FUNCTION__));
+    }
+
+    /**
+     * @return \Illuminate\Http\RedirectResponse
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function excelStore()
+    {
+        if (is_a($this->policy, Policy::class, true)) {
+            $this->authorize(__FUNCTION__, $this->entityName());
+        }
+
+        if (!request()->hasFile('excel')) {
+            return back();
+        }
+
+        $path = request()->file('excel')->getRealPath();
+
+        /** @var RowCollection $data */
+        $data = app('excel')
+            ->load($path)
+            ->get()
+            ->map(function ($value) {
+                return collect($this->getEntity()->getExcelAttributes())->map(function ($default, $attribute) use ($value) {
+                    return $value->{$attribute} ?? getExcelAttributesDefaultValue($default);
+                });
+            });
+
+//        dispatch(new ImportFromExcelJob($this->entityName(), $data->toArray()));
+
+        return information('L\'importation est en cours veuillez patienter');
+    }
+
+    /**
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function excelDownload()
+    {
+        if (is_a($this->policy, Policy::class, true)) {
+            $this->authorize(__FUNCTION__, $this->entityName());
+        }
+
+        $records = $this->getEntity()->get()->toArray();
+        $table = $this->getEntity()->getTable();
+
+        return app('excel')->create($table, function (LaravelExcelWriter $excel) use ($records, $table) {
+            $excel->sheet($table, function (LaravelExcelWorksheet $sheet) use ($records) {
+                $sheet->fromArray($records);
+            });
+        })->download();
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function activate(int $id)
+    {
+        if (is_a($this->policy, Policy::class, true)) {
+            $this->authorize(__FUNCTION__, $this->entityName());
+        }
+
+        /** @var Entity $entity */
+        $entity = $this->getEntity()->findOrFail($id);
+
+        if ($this->getService()->activate($entity)) {
+            return success('L\'Entité a été activée avec succès');
+        }
+
+        return error('Une erreur s\'est produite veuillez réessayer ultérieurement');
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function deactivate(int $id)
+    {
+        if (is_a($this->policy, Policy::class, true)) {
+            $this->authorize(__FUNCTION__, $this->entityName());
+        }
+
+        /** @var Entity $entity */
+        $entity = $this->getEntity()->findOrFail($id);
+
+        if ($this->getService()->deactivate($entity)) {
+            return success('L"Entité a été désactivée avec succès');
+        }
+
+        return error('Une erreur s\'est produite veuillez réessayer ultérieurement');
     }
 }
