acd33e9bdff514a182fc3e74778f1806a491e60e
Younes Khoubza <nesyou2013@gmail.com>
2018-01-08 13:46:05 +0000

    add yoeunes/toastr

--- src/LarafastServiceProvider.php
+++ src/LarafastServiceProvider.php
@@ -44,12 +44,12 @@ class LarafastServiceProvider extends ServiceProvider
         $this->app->register(\Barryvdh\Cors\ServiceProvider::class);
         $this->app->register(\Laravel\Passport\PassportServiceProvider::class);
         $this->app->register(\Collective\Html\HtmlServiceProvider::class);
-        $this->app->register(\Kamaln7\Toastr\ToastrServiceProvider::class);
         $this->app->register(\Spatie\Fractal\FractalServiceProvider::class);
         $this->app->register(\Spatie\MediaLibrary\MediaLibraryServiceProvider::class);
         $this->app->register(\Spatie\Permission\PermissionServiceProvider::class);
         $this->app->register(\Yajra\DataTables\DataTablesServiceProvider::class);
         $this->app->register(\Yajra\DataTables\HtmlServiceProvider::class);
+        $this->app->register(\Yoeunes\Toastr\ToastrServiceProvider::class);
     }
 
     private function registerAliases()
@@ -59,7 +59,7 @@ class LarafastServiceProvider extends ServiceProvider
             $loader->alias('Form', \Collective\Html\FormFacade::class);
             $loader->alias('Html', \Collective\Html\HtmlFacade::class);
             $loader->alias('Fractal', \Spatie\Fractal\FractalFacade::class);
-            $loader->alias('Toastr', \Kamaln7\Toastr\Facades\Toastr::class);
+            $loader->alias('Toastr', \Yoeunes\Toastr\Facades\Toastr::class);
         }
     }
 }
