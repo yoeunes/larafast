e7015f79632c7fe313456a9c4ce7cc4f4c384908
Younes Khoubza <nesyou2013@gmail.com>
2018-01-14 19:27:08 +0000

    clean up user entity

--- src/Entities/User.php
+++ src/Entities/User.php
@@ -10,14 +10,12 @@ use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
-use Spatie\MediaLibrary\HasMedia\Interfaces\HasMedia;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Entity implements
     AuthenticatableContract,
     AuthorizableContract,
-    CanResetPasswordContract,
-    HasMedia
+    CanResetPasswordContract
 {
     use Notifiable, Authenticatable, Authorizable, CanResetPassword, HasApiTokens, HasRoles;
 
