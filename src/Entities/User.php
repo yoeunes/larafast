4a6c7dd5395cd4d20a23650aaa66973db819732b
Younes Khoubza <nesyou2013@gmail.com>
2018-01-16 11:41:10 +0000

    Apply fixes from StyleCI

--- src/Entities/User.php
+++ src/Entities/User.php
@@ -5,12 +5,12 @@ namespace Yoeunes\Larafast\Entities;
 use Laravel\Passport\HasApiTokens;
 use Illuminate\Auth\Authenticatable;
 use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Entity implements
     AuthenticatableContract,
